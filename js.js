9f1bc7ec1682d06638a79215b6f08c696663c32e
Abhishek <chiseldart@hotmail.com>
2021-10-27 02:11:48 +0530

    bug remains: last move not showing

--- js.js
+++ js.js
@@ -1,6 +1,92 @@
-let input=document.querySelectorAll('input');
-console.log(input);
-input.forEach((value)=> {
-    console.log(value);
-    value.setAttribute('readonly');
+let symbol = "";
+let flag;
+
+let reset_func = () => {
+    input.forEach((i) => {
+
+        //re-enabling all buttons
+        i.disabled = false;
+
+        //clearing Xs & Os
+        i.value = "";
+
+        //resetting flag value
+        flag = 0;
+
+        set_flag();
+    });
+};
+
+//-----DECIDING FIRST SYMBOL
+let set_flag = () => {
+    flag = Math.floor(Math.random() * 2);
+    let turn = document.querySelector("#turn");
+    (flag % 2 == 0) ? turn.textContent = "X" : turn.textContent = "O";
+}
+
+//setting first symbol-value
+set_flag();
+
+//------CLICK TO GET X&O
+let input = document.querySelectorAll('input');
+input.forEach((j) => {
+
+    j.setAttribute('readOnly', true);
+    j.onclick = () => {
+
+        //getting current symbol
+        (flag % 2 == 0) ? symbol = "X" : symbol = "O";
+
+        //setting decided symbol
+        j.value = symbol;
+
+        //disabling field
+        j.setAttribute('disabled', 'true');
+
+        flag++;
+
+        if(game()){
+            j.style.color='rgb(255, 238, 0)';
+            alert("Player "+symbol+ " won");
+            reset_func();
+        }
+    }
 });
+
+
+//------RESET BUTTON
+let reset_button = document.querySelector('#reset');
+reset_button.onclick = reset_func;
+
+//GAME-LOGIC
+
+let game = () => {
+    
+    // WHEN 3 ADJACENT CUBES HV SAME VALUE, BUT NOT EMPTY
+    // ensuring that the common comparator is empty, prevents empty cubes from matching
+
+    if (input[0].value == input[1].value && input[2].value == input[1].value && input[2].value != "") {
+        return true;
+    }
+    else if (input[3].value == input[4].value && input[4].value == input[5].value && input[4].value != "") {
+        return true;
+    }
+    else if (input[6].value == input[7].value && input[7].value == input[8].value && input[7].value != "") {
+        return true;
+    }
+    else if (input[0].value == input[3].value && input[3].value == input[6].value && input[3].value != "") {
+        return true;
+    }
+    else if (input[1].value == input[4].value && input[4].value == input[7].value && input[4].value != "") {
+        return true;
+    }
+    else if (input[2].value == input[5].value && input[5].value == input[8].value && input[5].value != "") {
+        return true;
+    }
+    else if (input[0].value == input[4].value && input[4].value == input[8].value && input[4].value != "") {
+        return true;
+    }
+    else if (input[2].value == input[4].value && input[4].value == input[6].value && input[4].value != "") {
+        return true;
+    }
+}
